b13efebbe01a227c10c5c901cf40cc70c9ae8e34
Anthony Brignano <abrignano@comcast.net>
2023-03-31 15:30:47 -0400

    add `GameWrapperPokemonRed` to `game_wrappers`

--- pyboy/plugins/manager_gen.py
+++ pyboy/plugins/manager_gen.py
@@ -7,7 +7,7 @@ import re
 # Plugins and priority!
 # E.g. DisableInput first
 windows = ["WindowSDL2", "WindowOpenGL", "WindowHeadless", "WindowDummy", "Debug"]
-game_wrappers = ["GameWrapperSuperMarioLand", "GameWrapperTetris", "GameWrapperKirbyDreamLand"]
+game_wrappers = ["GameWrapperSuperMarioLand", "GameWrapperTetris", "GameWrapperKirbyDreamLand", "GameWrapperPokemonRed"]
 plugins = [
     "DisableInput", "AutoPause", "RecordReplay", "Rewind", "ScreenRecorder", "ScreenshotRecorder"
 ] + game_wrappers
