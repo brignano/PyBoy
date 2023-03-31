9a8273cb6a01eb4c75492bf41746c852d8f4e59f
Anthony Brignano <abrignano@comcast.net>
2023-03-31 12:14:18 -0400

    add basic unit test for pokemon red

---
+++ tests/test_game_wrapper_pokemon_red.py
@@ -0,0 +1,25 @@
+#
+# License: See LICENSE.md file
+# GitHub: https://github.com/Baekalfen/PyBoy
+#
+
+import os
+import platform
+import sys
+
+import numpy as np
+from pyboy import PyBoy, WindowEvent
+
+py_version = platform.python_version()[:3]
+is_pypy = platform.python_implementation() == "PyPy"
+
+
+def test_pokemon_red_basics():
+    pyboy = PyBoy("test_roms/pokemon.gb", window_type="dummy", game_wrapper=True)
+    pyboy.set_emulation_speed(0)
+    assert pyboy.cartridge_title() == "POKEMON RED"
+
+    pokemon_red = pyboy.game_wrapper()
+    pokemon_red.start_game()
+
+    pyboy.stop()
